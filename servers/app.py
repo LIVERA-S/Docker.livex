803f003e7261229b2715dd75c4c45b55221c6024
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-12-14 19:21:35 +0000

    heh

--- servers/app.py
+++ servers/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ping_server():
-    return "Welcome to the world of animals."
+    return "Welcome to the world of animals. animals animals animals animals"
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
