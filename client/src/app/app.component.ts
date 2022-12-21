6aaaf56f03e707d9902eed7f6c4ef8ecef4e6e78
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-12-21 13:01:55 +0000

    dodo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { AnimalsService } from './animals.service';
+import { Animal, VettAnimal } from './animals.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data = new Array<Animal>();
+  //Mi faccio iniettare l'animal servce
+  constructor(private animalService : AnimalsService)
+  {
+    //Mi sottoscrivo al servizio
+    this.animalService.getAnimals().subscribe(
+      (data: VettAnimal)=>{this.data = data['animals']}
+    )
+  }
 }
