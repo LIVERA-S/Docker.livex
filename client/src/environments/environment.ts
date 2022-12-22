5b6c6f2fc5676d67b51361680afa4190620a19c9
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-12-22 10:30:12 +0000

    hhe

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrlServer: 'TUO_SERVER_URL'
+    baseUrlServer: 'https://5000-liveras-dockerlivex-s3e3r0opmyy.ws-eu80.gitpod.io/animals'
   }
