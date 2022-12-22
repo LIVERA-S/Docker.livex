67206681b0c4e31fdbfa9661f75f751d706220a4
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-12-22 10:50:17 +0000

    hehe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrlServer: 'https://5000-liveras-dockerlivex-s3e3r0opmyy.ws-eu80.gitpod.io/animals'
+    baseUrlServer: 'https://5000-liveras-dockerlivex-scw8i8whb1b.ws-eu80.gitpod.io/'
   }
