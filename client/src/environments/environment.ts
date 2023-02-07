dedbede2060ee24d0be9470c1313686a1e5f6162
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2023-02-07 10:50:47 +0000

    Done NGNIX

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrlServer: 'https://5000-maliziag-dockergitpod-6ia3zbqltpp.ws-eu38.gitpod.io/'
+  baseUrlServer: 'https://5000-liveras-dockerlivex-2kdb0a4qvt4.ws-eu85.gitpod.io/'
 };
 
 /*
